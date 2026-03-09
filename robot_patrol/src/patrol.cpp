85ef2022d218e73ebd8a60b635f557b96059c944
Anna <anna.ying.yang@gmail.com>
2026-03-09 14:35:14 +0000

    Create subscription to laser data and implement logic to determine the safest direction to move.

---
+++ robot_patrol/src/patrol.cpp
@@ -0,0 +1,48 @@
+#include "rclcpp/create_subscription.hpp"
+#include "rclcpp/publisher.hpp"
+#include <functional>
+#include <rclcpp/rclcpp.hpp>
+include "sensor_msgs/msg/laser_scan.hpp"
+include "geometry_msgs/msg/twist.hpp"
+
+class Patrol:Node{
+    public:
+    Patrol::Patrol():Node("Patrol_node"){
+        twist_msg.linear.x = 0.1;
+        twist_msg.angular.z = 0.0;
+        direction_ = 0.0;
+        
+        auto qos = rclcpp::QoS(10).reliability(rclcpp::ReliabilityPolicy::Reliable);
+        scan_sub = rclcpp::create_subscription<sensor_msgs::msg::LaserScan>(
+            "/laser_scan",
+            qos,
+            std::bind(&Patrol::scan_callback, this, std::placeholders::_1)
+        );
+    };
+
+    private:
+    void scan_callback(sensor_msgs::msg::LaserScan::consPtr msg){
+        float min_distance = std::numeric_limits<float>::infinity();
+        int min_index;
+        for (size_t i = 0; i < msg->ranges.size(); i++){
+            if (msg->ranges[i] < range_min || msg->ranges[i] > range_max) continue; // discard invalid scans
+            if (msg->ranges[i] < min_distance) {
+                min_distance = msg->ranges[i];
+                min_index = i;
+            }
+        }
+
+        if (min_distance < 35){
+            direction_ = msg->angle_min + min_index * msg->angle_increment;
+        }else{
+            direction_ = 0;
+        }
+    };
+
+    // safest direction to move
+    float direction_;
+
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_sub;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr twist_pub;
+
+}
